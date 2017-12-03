3ac9713c0da5fb10cd8660087cd97233b7e33a9e
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-03 19:42:01 +0000

    Done.

--- lib/now_playing/cli.rb
+++ lib/now_playing/cli.rb
@@ -8,20 +8,27 @@ class NowPlaying::CLI
 
   def list_movies
     puts <<-DOC.gsub /^\s*/, ''
-      All the movies you want to see are listed here.
+      Rocky
+      All The Presidents menu
+      Pirates of the Caribbean
+      Rush
+      Alfie
+      The Pink Panther
     DOC
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the title of the film you're looking for or type exit to enter:"
+      puts "Enter the title of the film you're looking for or type movies for more titles or type exit:"
       input = gets.strip.downcase
       case input
       when "1"
         puts "Title of movie 1..."
       when "2"
         puts "Title of movie 2..."
+      when "movies"
+        list_movies
       end
   end
 end
