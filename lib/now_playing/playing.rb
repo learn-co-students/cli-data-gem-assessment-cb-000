eede493ae25043bb5471183d60c82ce72c001c57
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-03 20:46:46 +0000

    Done.

--- lib/now_playing/playing.rb
+++ lib/now_playing/playing.rb
@@ -3,15 +3,16 @@ class NowPlaying::Playing
   def self.today
     # Will pull movies playing today
     puts <<-DOC.gsub /^\s*/, ''
-      Rocky
-      All The Presidents Men
-      Pirates of the Caribbean
-      Rush
-      Alfie
-      The Pink Panther
+      Coco
+      Wonder
+      Justice League
+      Thor: Ragnarok
+      Wind River
+      The Disaster Artist
     DOC
     movie_1 = Movie.new
     movie_1.name = "Rocky"
-    movie_1
+    movie_1.theater = "Metro Four"
+    movie_1.url = "https://www.fandango.com/coco-2017-185799/movie-overview"
   end
 end
