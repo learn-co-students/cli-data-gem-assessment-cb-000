4455cb794d890b2918e63c6942cbb69bff024a36
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-03 19:31:00 +0000

    Done.

--- lib/now_playing/cli.rb
+++ lib/now_playing/cli.rb
@@ -2,6 +2,32 @@ class NowPlaying::CLI
 
   def call
     puts "Your Path to Hollywood:"
+    list_movies
+    menu
+  end
+
+  def list_movies
+    puts <<-DOC.gsub /^\s*/, ''
+      All the movies you want to see are listed here.
+    DOC
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the title of the film you're looking for or type exit to enter:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Title of movie 1..."
+      when "2"
+        puts "Title of movie 2..."
+      end
+  end
+end
+
+  def goodbye
+    puts "See you at the movies!"
   end
 
 end
